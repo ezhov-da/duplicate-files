abb84e119cd4af92c4dc0f5decffd945d6e9efe7
ezhov-da <ezhdenis@yandex.ru>
2019-09-15 10:50:45 +0300

    refactor: изменил способ запуска

--- app/run-gui.bat
+++ app/run-gui.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d %~dp0
-start "run" "%JAVA_HOME%\bin\javaw" -jar -Xmx2048m "duplicate-files-gui-swing.jar"
+start "run" "%JAVA_HOME%\bin\java" -jar -Xmx4096m "duplicate-files-gui-swing.jar"
